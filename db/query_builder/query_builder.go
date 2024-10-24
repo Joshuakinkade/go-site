4c5f7ad2d981494bb979706e5ddb2e019f2e2dc9
Josh Kinkade <joshmkinkade@gmail.com>
2024-10-23 22:08:09 -0600

    comments and cleanup

--- db/query_builder/query_builder.go
+++ db/query_builder/query_builder.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+// BuildUpdateClause creates the update clause of a sql statement from a map of fields and values.
+// It takes a slice of strings with the names of fields that are allowed in the update and returns
+// an error if there are fields in the input map that are not in the allowed fields slice. On
+// success, it returns the update clause as a string with parameter placeholders and a slice of with
+// the values. The allowed fields **must** be given in alphabetical order.
 func BuildUpdateClause(updates map[string]interface{}, allowedFields []string) (string, []interface{}, error) {
 	var wheres []string
 	var args []interface{}
