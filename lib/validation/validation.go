5c96e27af9a9b6bb9ba7c70532b9488bb70dc9c4
Josh Kinkade <joshmkinkade@gmail.com>
2024-10-24 18:44:41 -0600

    added first validation types

--- lib/validation/validation.go
+++ lib/validation/validation.go
@@ -1,32 +1,87 @@
 package validation
 
 import (
+	"fmt"
 	"regexp"
 )
 
-type Example struct {
-	FirstName string `validate:"required"`
-}
-
-type ValidationError struct {
-	Field   string
-	Message string
+type Validator interface {
+	Validate(interface{}) error
 }
 
 type StringValidator struct {
-	regex     *regexp.Regexp
 	maxLength int
 	minLength int
+	regexp    *regexp.Regexp
+	required  bool
 }
 
-func NewStringValidator(regex string, minLength, maxLength int) StringValidator {
-	return StringValidator{
-		regex:     regexp.MustCompile(regex),
-		maxLength: maxLength,
-		minLength: minLength,
+func String() StringValidator {
+	return StringValidator{}
+}
+
+func (v StringValidator) MaxLength(maxLength int) StringValidator {
+	v.maxLength = maxLength
+	return v
+}
+
+func (v StringValidator) MinLength(minLength int) StringValidator {
+	v.minLength = minLength
+	return v
+}
+
+func (v StringValidator) Regexp(regexp *regexp.Regexp) StringValidator {
+	v.regexp = regexp
+	return v
+}
+
+func (v StringValidator) Validate(input interface{}) error {
+	switch input := input.(type) {
+	case string:
+		if len(input) < v.minLength {
+			return fmt.Errorf("value is too short")
+		} else if v.maxLength > 0 && len(input) > v.maxLength {
+			return fmt.Errorf("value is too long")
+		} else if v.regexp != nil && !v.regexp.Match([]byte(input)) {
+			return fmt.Errorf("value does not match pattern")
+		}
+	default:
+		return fmt.Errorf("value is not a string")
+	}
+
+	return nil
+}
+
+type MapValidator struct {
+	validators map[string]Validator
+}
+
+func Map() MapValidator {
+	return MapValidator{
+		validators: map[string]Validator{},
 	}
 }
 
-func ValidateStruct(val interface{}) []ValidationError {
+func (v MapValidator) Add(key string, validator Validator) MapValidator {
+	v.validators[key] = validator
+	return v
+}
+
+func (v MapValidator) Validate(input interface{}) error {
+	switch input := input.(type) {
+	case map[string]interface{}:
+		for field, value := range input {
+			validator, ok := v.validators[field]
+			if !ok {
+				return fmt.Errorf("field %s is not known", field)
+			}
+			err := validator.Validate(value)
+			if err != nil {
+				return fmt.Errorf("field %s: %s", field, err.Error())
+			}
+		}
+	default:
+		return fmt.Errorf("value is not a map")
+	}
 	return nil
 }
--- lib/validation/validation_test.go
+++ lib/validation/validation_test.go
@@ -1,19 +1,59 @@
 package validation_test
 
 import (
+	"regexp"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/joshuakinkade/go-site/lib/validation"
 )
 
-type TestStruct struct {
-}
-
 var _ = Describe("Validation", func() {
-	It("should not return errors for a valid struct", func() {
-
-		err := validation.ValidateStruct(ex)
-		Expect(err).To(BeNil())
+	Describe("StringValidator", func() {
+		It("should not reject a valid string", func() {
+			v := validation.String().MinLength(1).MaxLength(5).Regexp(regexp.MustCompile(".*"))
+			Expect(v.Validate("hello")).To(BeNil())
+		})
+		It("should reject non-string value", func() {
+			v := validation.String()
+			Expect(v.Validate(123)).To(MatchError("value is not a string"))
+		})
+		It("should reject a short string", func() {
+			v := validation.String().MinLength(5).MaxLength(10).Regexp(regexp.MustCompile(".*"))
+			Expect(v.Validate("hi")).To(MatchError("value is too short"))
+		})
+		It("should reject a long string", func() {
+			v := validation.String().MinLength(1).MaxLength(5).Regexp(regexp.MustCompile(".*"))
+			Expect(v.Validate("hello, world!")).To(MatchError("value is too long"))
+		})
+		It("should reject a string that doesn't match the regexp", func() {
+			v := validation.String().MinLength(1).MaxLength(10).Regexp(regexp.MustCompile("^[A-z]+$"))
+			Expect(v.Validate("hello123")).To(MatchError("value does not match pattern"))
+		})
+		It("should ignore empty regexp", func() {
+			v := validation.String().MaxLength(1).MaxLength(6)
+			Expect(v.Validate("hello")).To(BeNil())
+		})
+		It("should ignore empty (0) minLength", func() {
+			v := validation.String().MaxLength(6).Regexp(regexp.MustCompile(".*"))
+			Expect(v.Validate("")).To(BeNil())
+		})
+		It("should ignore empty (0) maxLength", func() {
+			v := validation.String().MinLength(1).Regexp(regexp.MustCompile(".*"))
+			Expect(v.Validate("hello")).To(BeNil())
+		})
+	})
+	Describe("MapValidator", func() {
+		It("should validate its children", func() {
+			v := validation.Map()
+			v.Add("name", validation.String().MinLength(1))
+			Expect(v.Validate(map[string]interface{}{"name": "hello"})).To(BeNil())
+		})
+		It("should return it's childrens' errors", func() {
+			v := validation.Map()
+			v.Add("name", validation.String().MinLength(1).MaxLength(2))
+			Expect(v.Validate(map[string]interface{}{"name": "hello"})).To(MatchError("field name: value is too long"))
+		})
 	})
 })
